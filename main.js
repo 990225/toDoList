850e317e871a26d406176e86bb7f1add78540042
990225 <hj6561@naver.com>
2020-10-24 12:18:19 +0900

    인풋 버튼 클릭 코드 작성

---
+++ main.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const toDoInputBtn = document.querySelector(".toDo__input__btn");
+
+toDoInputBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const toDoInputItem = document.querySelector(".toDo__input__item"),
+    toDoOutput = document.querySelector(".toDo__output"),
+    toDoNewForm = document.createElement("form"),
+    toDoNewInput = document.createElement("input"),
+    toDoNewDiv = document.createElement("div"),
+    toDoNewEditBtn = document.createElement("button"),
+    toDoNewCompleteBtn = document.createElement("button"),
+    toDoNewRemoveBtn = document.createElement("button");
+
+  if (toDoInputItem.value) {
+    toDoOutput.appendChild(toDoNewForm);
+
+    toDoNewForm.className = "toDo__output__container";
+    toDoNewForm.appendChild(toDoNewInput);
+    toDoNewForm.appendChild(toDoNewDiv);
+
+    toDoNewInput.className = "toDo__output__item";
+    toDoNewInput.setAttribute("type", "text");
+    toDoNewInput.setAttribute("disabled", "true");
+    toDoNewInput.value = toDoInputItem.value;
+
+    toDoInputItem.value = "";
+    toDoInputItem.focus();
+
+    toDoNewDiv.className = "toDo__output__item__btn";
+    toDoNewDiv.appendChild(toDoNewEditBtn);
+    toDoNewDiv.appendChild(toDoNewCompleteBtn);
+    toDoNewDiv.appendChild(toDoNewRemoveBtn);
+
+    toDoNewEditBtn.className = "toDo__output__item__btn-edit";
+    toDoNewEditBtn.innerHTML = "<i class='fas fa-pen-square'></i>";
+
+    toDoNewCompleteBtn.className = "toDo__output__item__btn-complete";
+    toDoNewCompleteBtn.innerHTML = "<i class='fas fa-check-square'></i>";
+
+    toDoNewRemoveBtn.className = "toDo__output__item__btn-remove";
+    toDoNewRemoveBtn.innerHTML = "<i class='fas fa-minus-square'></i>";
+  }
+});
